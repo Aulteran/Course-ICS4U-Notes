2453eee0720050a36b96fe878ec0bad6a7a266f5
Aadil Hussain <ault@andrims.com>
2023-09-12 15:02:24 +0000

    sept12 in-class notes

---
+++ random notes.py
@@ -0,0 +1,14 @@
+# rand array
+arr = [[2,5,8],
+       [3,7,4],
+       [1,6,9]]
+
+dicti = {
+    "a": {"x": 54, "y": 82, "z": 91},
+    "b": {"x": 75, "y": 29, "z": 80}
+}
+
+NYC = {'location': (40.7128, 74.0059),
+       "celebs": ["Jay Z", "K. Bacon", "Nas"],
+       "facts": {'state': "NY", 'country': 'America'}
+       }
