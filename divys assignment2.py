06468a407780026821bd28d5c8d07915274f1a1a
Ault <ault@andrims.com>
2023-09-20 22:36:30 -0400

    fixed divys code

---
+++ divys assignment2.py
@@ -0,0 +1,18 @@
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+message = 'guv6Jv6Jz!J6rp5r7Jzr66ntrM'
+number = 1
+while number < len(SYMBOLS):
+    mystr = ""
+    for letter in message:
+        index = SYMBOLS.index(letter)
+        if index+number>65:
+            remainder = index+number-65
+            if remainder==0:
+                mystr+=SYMBOLS[0]
+            else:      
+                mystr+=SYMBOLS[remainder]
+        else:
+            index+=number
+            mystr+=(SYMBOLS[index])
+    print(mystr)
+    number+=1
