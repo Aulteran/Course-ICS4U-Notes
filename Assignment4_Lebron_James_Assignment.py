2dadd716da64cd7e63c4a9634c923f1e043d3f2c
Ault <ault@andrims.com>
2023-11-10 23:35:30 -0500

    completed logic part of Lebron Assignment

---
+++ Assignment4_Lebron_James_Assignment.py
@@ -0,0 +1,126 @@
+'''
+Author: Aadil Hussain
+Built on: Python 3.10.11
+'''
+
+# You have been asked to create a program which allows head NBA scouts to quickly and 
+# efficiently sort through data on any given player in the NBA. The scout has asked that you
+# use Lebron James from the Los Angeles Lakers as your test subject.
+
+# Lebron has had a successful career as an NBA superstar, and played for a number of teams while 
+# in the NBA. As such, there is a considerable amount of data to course through – and scouts DON’T want 
+# to have to do this. You will find Lebron’s regular season data on the following page.
+# Please create a program using a functional framework that allows the head scouts to determine
+# key statistics of players.
+
+# WHAT DO THE SCOUTS WANT TO KNOW IN THE PROGRAM?? (IN EACH INSTANCE CREATE A NEW DATA STRUCTURE TO DISPLAY INFORMATION
+    # 1) Which years did Lebron average more than 28 ppg
+    # 2) Which years was Lebron’s field goal percentage over .500 
+    # 3) When Lebron played for Cleveland, what years was his free throw (FT) percentage over .720
+    # 4) When Lebron played for Miami (MIA) , what was his average field goal percentage (FG)
+    # 5) What is the TOTAL number of games (G) Lebron has played in his career
+    # 6) A regular season has 82 games – how many games did he miss each season? (season, number of games missed)
+    # 7) What percent of games did Lebron start in comparison to games played?
+    # 8) How many years has Lebron played in the NBA?
+    # 9) Which position (POS) was Lebron’s Assists per game average (AST) highest? (PG,SG,SF,PF)
+
+# PLAYOFF LEBRON VS. REG. SEASON LEBRON (Hint: you might want to add an additional column stat to the reg season tuple)
+    # 1) What is the total number of games (G) Lebron has played in the playoffs?
+    # 2) What is Lebron’s average FG attempts per game in the playoffs?
+    # 3) IN THE SEASONS WHICH LEBRON MADE THE PLAYOFFS ONLY: was his average FG% higher in the regular season or the playoffs?
+    # 4) IN THE SEASONS WHICH LEBRON MADE THE PLAYOFFS ONLY: create a new data set comparing minutes played in regular season vs. playoffs (per season)
+    # 5) IN THE SEASONS WHICH LEBRON MADE THE PLAYOFFS ONLY: what season was his FG% above .500 in BOTH the regular season AND playoffs.
+
+# Make the results look pretty for the scouts by using pprint – Also – if you want, for relevant answers you can also print a summary statement (ie. Q 9 – you should show 
+# the data structures grouped by POS – have a final statement after stating – Lebron’s assists per game average was highest as a POS
+
+import collections
+from functools import reduce
+from pprint import pprint
+from statistics import mean
+
+Lebron = collections.namedtuple('Lebron',['Season','Age','Team','Lg','Pos','G','GS','MP','FG','FGA','FG_per','three_P','three_PA','three_P_per','two_P','two_PA','two_P_per',
+'eFG_per','FT','FTA','FT_per','ORB','DRB','TRB','AST','STL','BLK','TOV','PF','PTS'])
+
+lebron_data = (
+    Lebron('2003-04',19,'CLE','NBA','SG',79,79,39.5,7.9,18.9,.417,0.8,2.7,.290,7.1,16.1,.438,.438,4.4,5.8,.754,1.3,4.2,5.5,5.9,1.6,0.7,3.5,1.9,20.9),
+    Lebron('2004-05',20,'CLE','NBA','SF',80,80,42.4,9.9,21.1,.472,1.4,3.9,.351,8.6,17.2,.499,.504,6.0,8.0,.750,1.4,6.0,7.4,7.2,2.2,0.7,3.3,1.8,27.2),
+    Lebron('2005-06',21,'CLE','NBA','SF',79,79,42.5,11.1,23.1,.480,1.6,4.8,.335,9.5,18.3,.518,.515,7.6,10.3,.738,0.9,6.1,7.0,6.6,1.6,0.8,3.3,2.3,31.4),
+    Lebron('2006-07',22,'CLE','NBA','SF',78,78,40.9,9.9,20.8,.476,1.3,4.0,.319,8.6,16.8,.513,.507,6.3,9.0,.698,1.1,5.7,6.7,6.0,1.6,0.7,3.2,2.2,27.3),
+    Lebron('2007-08',23,'CLE','NBA','SF',75,74,40.4,10.6,21.9,.484,1.5,4.8,.315,9.1,17.1,.531,.518,7.3,10.3,.712,1.8,6.1,7.9,7.2,1.8,1.1,3.4,2.2,30.0),
+    Lebron('2008-09',24,'CLE','NBA','SF',81,81,37.7,9.7,19.9,.489,1.6,4.7,.344,8.1,15.2,.535,.530,7.3,9.4,.780,1.3,6.3,7.6,7.2,1.7,1.1,3.0,1.7,28.4),
+    Lebron('2009-10',25,'CLE','NBA','SF',76,76,39.0,10.1,20.1,.503,1.7,5.1,.333,8.4,15.0,.560,.545,7.8,10.2,.767,0.9,6.4,7.3,8.6,1.6,1.0,3.4,1.6,29.7),
+    Lebron('2010-11',26,'MIA','NBA','SF',79,79,38.8,9.6,18.8,.510,1.2,3.5,.330,8.4,15.3,.552,.541,6.4,8.4,.759,1.0,6.5,7.5,7.0,1.6,0.6,3.6,2.1,26.7),
+    Lebron('2011-12',27,'MIA','NBA','SF',62,62,37.5,10.0,18.9,.531,0.9,2.4,.362,9.1,16.5,.556,.554,6.2,8.1,.771,1.5,6.4,7.9,6.2,1.9,0.8,3.4,1.5,27.1),
+    Lebron('2012-13',28,'MIA','NBA','PF',76,76,37.9,10.1,17.8,.565,1.4,3.3,.406,8.7,14.5,.602,.603,5.3,7.0,.753,1.3,6.8,8.0,7.3,1.7,0.9,3.0,1.4,26.8),
+    Lebron('2013-14',29,'MIA','NBA','PF',77,77,37.7,10.0,17.6,.567,1.5,4.0,.379,8.5,13.6,.622,.610,5.7,7.6,.750,1.1,5.9,6.9,6.3,1.6,0.3,3.5,1.6,27.1),
+    Lebron('2014-15',30,'CLE','NBA','SF',69,69,36.1,9.0,18.5,.488,1.7,4.9,.354,7.3,13.6,.536,.535,5.4,7.7,.710,0.7,5.3,6.0,7.4,1.6,0.7,3.9,2.0,25.3),
+    Lebron('2015-16',31,'CLE','NBA','SF',76,76,35.6,9.7,18.6,.520,1.1,3.7,.309,8.6,14.9,.573,.551,4.7,6.5,.731,1.5,6.0,7.4,6.8,1.4,0.6,3.3,1.9,25.3),
+    Lebron('2016-17',32,'CLE','NBA','SF',74,74,37.8,9.9,18.2,.548,1.7,4.6,.363,8.3,13.5,.611,.594,4.8,7.2,.674,1.3,7.3,8.6,8.7,1.2,0.6,4.1,1.8,26.4),
+    Lebron('2017-18',33,'CLE','NBA','PF',82,82,36.9,10.5,19.3,.542,1.8,5.0,.367,8.6,14.3,.603,.590,4.7,6.5,.731,1.2,7.5,8.6,9.1,1.4,0.9,4.2,1.7,27.5),
+    Lebron('2018-19',34,'LAL','NBA','SF',55,55,35.2,10.1,19.9,.510,2.0,5.9,.339,8.1,14.0,.582,.560,5.1,7.6,.665,1.0,7.4,8.5,8.3,1.3,0.6,3.6,1.7,27.4),
+    Lebron('2019-20',35,'LAL','NBA','PG',67,67,34.6,9.6,19.4,.493,2.2,6.3,.348,7.4,13.1,.564,.550,3.9,5.7,.693,1.0,6.9,7.8,10.2,1.2,0.5,3.9,1.8,25.3),
+    Lebron('2020-21',36,'LAL','NBA','PG',43,43,33.7,9.3,18.1,.513,2.3,6.4,.366,7.0,11.7,.594,.578,4.1,5.8,.701,0.6,7.3,7.9,7.8,1.0,0.6,3.8,1.6,25.0)
+)
+
+pprint (lebron_data)
+
+# Question 1
+pprint(list(filter(lambda season: season[9] > 28, lebron_data)))
+
+# Question 2
+pprint(list(filter(lambda season: season[11] > .500, lebron_data)))
+
+# Question 3
+pprint(list(filter(lambda season: season[2] == 'CLE' and season[21] > .720, lebron_data)))
+
+# Question 4
+mia_fg_percentages = list(map(lambda season: season[9], filter(lambda season: season[2] == 'MIA', lebron_data)))
+pprint(mean(mia_fg_percentages))
+
+# Question 5
+total_games = reduce(lambda acc, season: acc + season[5], lebron_data, 0)
+print(total_games)
+
+# Question 6
+missed_games_per_season = list(map(lambda season: (season[0], 82 - season[5]), lebron_data))
+pprint(missed_games_per_season)
+
+# Question 7
+start_percentage = reduce(lambda acc, season: acc + season[6] / season[5], lebron_data, 0) / len(lebron_data) * 100
+print(f"LeBron started {start_percentage:.2f}% of the games.")
+
+# Question 8
+years_played = len(set(map(lambda season: season[0][:4], lebron_data)))
+print(f"LeBron has played in the NBA for {years_played} years.")
+
+# Question 9
+positions = {'PG': [], 'SG': [], 'SF': [], 'PF': []}
+for season in lebron_data:
+    positions[season[4]].append(season[24])
+pprint(positions)
+
+# PLAYOFF vs. REG QUESTIONS
+# Question 1
+playoff_games = reduce(lambda acc, season: acc + season[5], filter(lambda season: season[3] == 'NBA', lebron_data), 0)
+print(f"LeBron has played {playoff_games} games in the playoffs.")
+
+# Question 2
+playoff_fg_attempts_per_game = reduce(lambda acc, season: acc + season[9] / season[5], filter(lambda season: season[3] == 'NBA', lebron_data), 0)
+print(f"LeBron's average FG attempts per game in the playoffs: {playoff_fg_attempts_per_game:.2f}")
+
+# Question 3
+regular_season_fg_percentages = list(map(lambda season: season[9], filter(lambda season: season[3] == 'NBA', lebron_data)))
+playoff_fg_percentages = list(map(lambda season: season[9], filter(lambda season: season[3] == 'NBA', lebron_data)))
+if mean(regular_season_fg_percentages) > mean(playoff_fg_percentages):
+    print("LeBron's average FG% is higher in the regular season.")
+else:
+    print("LeBron's average FG% is higher in the playoffs.")
+
+# Question 4
+minutes_comparison = list(map(lambda season: (season[0], season[7], season[25]), filter(lambda season: season[3] == 'NBA', lebron_data)))
+pprint(minutes_comparison)
+
+# Question 5
+fg_above_500_seasons = list(map(lambda season: season[0], filter(lambda season: season[9] > .500 and season[21] > .500, lebron_data)))
+pprint(fg_above_500_seasons)
