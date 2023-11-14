2e39a13497d75e953d7633a41d27f3606b645b57
Ault <ault@andrims.com>
2023-11-14 11:28:32 -0500

    Added OPP Quiz Inst

---
+++ OOP_Quiz.py
@@ -0,0 +1,19 @@
+'''
+Author: Aadil Hussain
+Built on: Python 3.10.11
+'''
+
+# A fellow programmer has asked that you help them code their game. 
+# They have asked that you model a real world object using OOP in order 
+# that they may use the class over and over.
+
+# Please create class for a speaker. He needs the speaker to :
+    # turn on and off
+    # have 2 inputs (that be toggled between)
+    # have volume levels from 1 to 5
+    # must be able to check all of this information using a
+    # have a mute button (independant of volume level)
+    # NOTE: make sure to do some planning!!! HINT: ask yourself 
+    # what PROPERTIES the speaker will have, and what BEHAVIOURS it should have.
+
+# There is a set of test code that is attached to this git. You will run the test code on your class
