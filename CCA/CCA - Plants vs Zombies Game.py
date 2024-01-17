a9dba2e90a30879522a065a967c3466d5282b450
Aadil Hussain <ault@andrims.com>
2024-01-17 08:57:34 -0500

    Peashooter obj -> sprite + user stat vars

--- CCA/CCA - Plants vs Zombies Game.py
+++ CCA/CCA - Plants vs Zombies Game.py
@@ -59,24 +59,39 @@ class Player():
         self.wallet = start_money
         self.num_plants = 0 # max 5 plants
         self.plants = []
+        self.plantsgrouped = pygame.sprite.Group()
+        self.shot_strength = 1 # levels 1 to 3
+        self.zombies_killed = 0
+        self.superzombies_killed = 0
+        self.shots_made = 0
+        self.max_strength_unlocked = 1
+        self.speed_booster_unlocked = 1
     
     def add_plant(self, spawn_coords):
+        if self.num_plants >= 5:
+            print("Cannot add additional plants! Max of 5 Plants")
         # create peashooter instance
         self.plants.append(Peashooter(spawn_coords[0], spawn_coords[1], self))
         self.num_plants += 1
         print(f"Created Plant ID {self.num_plants}")
-        # create enemy zombie
         active_plant:Peashooter = self.plants[self.num_plants-1]
+
+        # add plant to sprite group for easy updates and drawings
+        self.plantsgrouped.add(active_plant)
+
+        # create enemy zombie
         active_plant.add_enem_zombie()
 
 
 # Peashooter class
-class Peashooter():
+class Peashooter(pygame.sprite.Sprite):
     def __init__(self, x, y, player:Player, plantID = 0, image_path = "CCA\\assets\\images\\plants\\peashooter.png"):
-        self.x = x
-        self.y = y
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(image_path)
         self.image = pygame.transform.scale(self.image, (60,100))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x + 25
+        self.rect.centery = y + 60
         self.enemies = []
         self.enemy_spawnpoint = ZOMBIE_SPAWNPOINTS[player.num_plants] # tuple (x, y)
         self.plantID = plantID
@@ -86,39 +101,37 @@ class Peashooter():
         self.shots = []
         self.peashots = pygame.sprite.Group()
         self.enemiesgrouped = pygame.sprite.Group()
-
-    def draw(self):
-        screen.blit(self.image, (self.x, self.y))
     
     def add_enem_zombie(self, spawnpoint=0):
         self.shooting = False
         if spawnpoint == 0:
             spawnpoint = self.enemy_spawnpoint
-        self.enemies.append(Zombie(spawnpoint[0], spawnpoint[1], random.uniform(0.5, 2)))
+        self.enemies.append(Zombie(spawnpoint[0], spawnpoint[1]+15, random.uniform(0.5, 2)))
         self.enemiesgrouped.add(self.enemies[-1])
         print(f"Created Zombie {self.enemies[-1]}")
         self.shooting = True
     
     def shoot(self, spawnpoint=0):
         if spawnpoint==0:
-            spawnpoint = (self.x, self.y)
+            spawnpoint = (self.rect.centerx, self.rect.centery)
         self.shots.append(Pea(spawnpoint[0], spawnpoint[1], angle=0))
         self.peashots.add(self.shots[0])
 
 class Pea(pygame.sprite.Sprite):
-    def __init__(self, x, y, angle = 0, image_path = "CCA\\assets\\images\\projectiles\\pea.png"):
+    def __init__(self, x, y, angle = 0, speed = 2, image_path = "CCA\\assets\\images\\projectiles\\pea.png"):
         pygame.sprite.Sprite.__init__(self)
         # Load, transform, and get rect of image
         self.image = pygame.image.load(image_path)
         self.image = pygame.transform.scale(self.image, (30,30))
         self.rect = self.image.get_rect()
-        # Set init pos and angle
+        # Set init pos, speed, and angle
         self.rect.centerx = x + 30
-        self.rect.centery = y + 70
+        self.rect.centery = y + 30
+        self.speedconst = speed
         self.angle = angle
         # Set speed based off angle, will prolly be 0 degrees for PvZ
-        self.speedx = 5 * math.cos(math.radians(self.angle))
-        self.speedy = -5 * math.sin(math.radians(self.angle))
+        self.speedx = self.speedconst * math.cos(math.radians(self.angle))
+        self.speedy = -(self.speedconst) * math.sin(math.radians(self.angle))
     
     def update(self): 
         self.rect.x += self.speedx
@@ -136,8 +149,10 @@ class Zombie(pygame.sprite.Sprite):
         self.rect.centerx = x
         self.rect.centery = y + 40
         self.speed = speed
-        # set health
-        self.hp = 100
+        # set default health or superzombie x2 health
+        self.hp = 100 # can be 200hp 1/5th chance
+        if random.randint(1,5) == 1:
+            self.hp *= 2
     
     def damaged(self, damage_dealt):
         self.hp -= damage_dealt
@@ -180,8 +195,10 @@ while True:
     # add "dropshadow" to increase sprite visibility
     dropshadow.draw()
 
-    # check zombie healths
+    main_player.plantsgrouped.update()
+    main_player.plantsgrouped.draw(screen)
 
+    # zombie list
     zombies = []
     zombie:Zombie
  
@@ -205,14 +222,13 @@ while True:
             plant.shoot()
         plant.shooting = False
 
+        # update and draw zombies
         plant.enemiesgrouped.update()
         plant.enemiesgrouped.draw(screen)
 
+        # update and draw shots
         plant.peashots.update() # cant use any asyncio sleeps for smooth animation
         plant.peashots.draw(screen)
-
-        # draw plants
-        plant.draw()
 
     pygame.display.flip() # pygame window mainloop
 
